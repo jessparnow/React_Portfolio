b2b217b9a834ea74d51b27593462ebb4093923fb
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-06 21:04:22 -0600

    Adding useState

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,19 +1,27 @@
 import React, {useState} from "react";
 import projects from "../portfolio.json"
+import workout from "../images/workouttracker.png"
 
 function Portfolio () {
+    // let projects = props.projects;
+
     const [developerState] = useState({
-        projects
+        projects,
+        name: "Workout Tracker",
+        description: "App to keep track of daily workouts and data over time.",
+        github: "https://github.com/jessparnow/WorkoutTracker",
+        app_page: "https://limitless-bastion-88861.herokuapp.com/?id=60208d34225a5100153538c1"
       });
     return (
        
    
 <div className="card">
-  <img src={developerState.image} className="card-img-top" alt="..." />
+  <img src={workout} className="card-img-top" alt="..." style={{width:"18rem"}} />
   <div className="card-body">
     <h5 className="card-title">{developerState.name}</h5>
     <p className="card-text">{developerState.description}</p>
-    <a href={developerState.github} className="btn btn-primary">Go somewhere</a>
+    <a href={developerState.github} className="btn btn-primary">GitHub</a>
+    <a href={developerState.app_page} className="btn btn-primary">App Page</a>
   </div>
 </div>
     )
