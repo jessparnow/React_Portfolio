34a4ad1427d5fe67fc94f2080ebae3ffb299a640
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-10 20:24:48 -0600

    couldn't get useState to work so switched to props

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -1,25 +1,24 @@
 import React from "react";
-import projects from "../portfolio.json";
 
-function ProjectsCard() {
-  let developerState = projects;
-  console.log(developerState)
+function ProjectsCard(props) {
+ 
+ console.log(props);
   return (
     
     <div className="card">
       <img
-        src={developerState.image}
+        src={props.image}
         className="card-img-top"
         alt="..."
         style={{ width: "18rem" }}
       />
       <div className="card-body">
-        <h5 className="card-title">{developerState.name}</h5>
-        <p className="card-text">{developerState.description}</p>
-        <a href={developerState.github} className="btn btn-primary">
+        <h5 className="card-title">{props.name}</h5>
+        <p className="card-text">{props.description}</p>
+        <a href={props.github} className="btn btn-primary">
           GitHub
         </a>
-        <a href={developerState.app_page} className="btn btn-primary">
+        <a href={props.app_page} className="btn btn-primary">
           App Page
         </a>
       </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import ProjectsCard from "../components/ProjectsCard";
 import projects from "../portfolio.json";
 
-function Portfolio() {
-  let developerState = useState({
-   projects
-  });
-  //console.log(developerState);
-  return (
-    <div>
-      {developerState.map(() => (
-      <ProjectsCard
-        
-          name={projects.name}
-          image={projects.image}
-          description={projects.description}
-          github={projects.github}
-          app_page={projects.app_page}
-        
-      />
-      ))}
-    </div>
-  );
+class Portfolio extends Component {
+  state = {
+    projects,
+  };
+  render() {
+    
+    return (
+      <div>
+        {this.state.projects.map((projects) => (
+          <ProjectsCard
+      
+            name={projects.name}
+            image={projects.image}
+            description={projects.description}
+            github={projects.github}
+            app_page={projects.app_page}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 export default Portfolio;
