3fd6ce34bd04f804ec3ba6cbe2026d07868e9074
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-05-22 18:55:13 -0500

    Added new project and updated colors

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,9 +6,9 @@ function Contact () {
     <div>
         <h1 className="contact-header">Thank you for checking out my portfolio.</h1>
         <p className="contact-header">For Inquiries, please email me at jessparnow@gmail.com</p>
-        <a href="https://docs.google.com/document/d/1uFplgwEw_nSzWaoLBWzQKHOnNXjzyE7OMwyby2K3iP4/edit?usp=sharing" className="btn btn-outline-light">Resume</a>
-        <a href="https://www.linkedin.com/in/jessica-parnow-4626221b9/" className="btn btn-outline-light">LinkedIn Profile</a>
-        <a href="https://github.com/jessparnow" className="btn btn-outline-light">GitHub Profile</a>
+        <a href="https://docs.google.com/document/d/1uFplgwEw_nSzWaoLBWzQKHOnNXjzyE7OMwyby2K3iP4/edit?usp=sharing" className="btn btn-outline-info">Resume</a>
+        <a href="https://www.linkedin.com/in/jessica-parnow-4626221b9/" className="btn btn-outline-info">LinkedIn Profile</a>
+        <a href="https://github.com/jessparnow" className="btn btn-outline-info">GitHub Profile</a>
     </div>
     )
 }
